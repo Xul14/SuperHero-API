6c8441f4a0022574fdacf2f46c472051caa2148b
Julia <juliasoaresdealmeidaj@gmail.com>
2023-05-02 11:13:35 -0300

    Barra de pesquisa e card

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const routes = {
-    '/favorites': '/pages/favorites.html'
+    '/favorites': '/pages/favorites.html',
 }
 
 const route = async() => {
@@ -18,3 +18,26 @@ const route = async() => {
 }
 
 window.route = route
+
+
+const routes_container= {
+    '/powerstats': '/pages/powerstats.html',
+    '/biography': '/pages/biography.html',
+    '/appearance': '/pages/appearance.html',
+    '/connections': '/pages/connections.html'
+}
+
+const route_containers = async() => {
+    window.event.preventDefault()
+    window.history.pushState({}, "", window.event.target.href)
+
+    const path = window.location.pathname
+
+    const response = await fetch(routes_container[path])
+    const html = await response.text()
+
+    document.getElementById('content_pages').innerHTML = html
+
+}
+
+window.route = route_containers
